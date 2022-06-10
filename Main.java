f7ef6c13e1e93255440480fd005e1e202b6547ed
Budhi <gurung.b1t@gmail.com>
2022-06-10 11:00:44 -0400

    changed to LinkedList

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Main{
         public static void main(String[] args) {
-        ArrayList<StudentData> sd = new ArrayList<>();
+        LinkedList<StudentData> sd = new LinkedList<>();
+    
         sd.add(new StudentData(1, 3.5, "Sam", 20, "Computer Science"));
 
-        ArrayList<TeacherData> td = new ArrayList<>();
+        LinkedList<TeacherData> td = new LinkedList<>();
         td.add(new TeacherData(1, "Douglus", 32, "Computer Science"));
 
        Student s = new Student(sd.get(0)); 
